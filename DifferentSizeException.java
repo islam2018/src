1b570834f1002f9d5d9bfad5974d1ba9a5b17777
amira96 <fa_aiouez@esi.dz>
2017-10-18 21:29:16 +0100

    Add files via upload

---
+++ DifferentSizeException.java
@@ -0,0 +1,12 @@
+package code;
+
+public class DifferentSizeException extends Exception {
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Les deux vecteurs sont de taille différentes .";
+	}
+	
+
+}
---
+++ MinMax.java
@@ -0,0 +1,11 @@
+package code;
+
+public class MinMax {
+	protected int min ;
+	protected int max  ;
+	
+ public MinMax (int min , int max  ) {
+	 this.min = min ;
+	 this.max = max;
+ }
+}
